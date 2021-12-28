d5bf84240d8f73304f94fa9c60183bdd0cfae7c9
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-29 03:54:59 +0530

    ignore committed code dir

--- code/deploy.sh
+++ code/deploy.sh
@@ -11,6 +11,7 @@ git checkout -b $temp_branch
 ignorecontent="code/"
 cd .. && echo "$ignorecontent" > .gitignore
 # TODO: ignore already committed 'code' dir
+git rm -r code/
 git add .
 git commit -m "Gatsby build ${date_time}"
 git push origin $temp_branch
@@ -18,7 +19,7 @@ echo "Pushed changes to temp branch..."
 git checkout gatsby
 # TODO: merge temp branch to main here
 echo "Deleting temp branch..."
-git push origin --delete $temp_branch
+#git push origin --delete $temp_branch --commented for testing purposes only
 echo "Deleted temp branch, deleting all local generated files"
 shopt -s extglob
 rm -vr !(code|.git)
