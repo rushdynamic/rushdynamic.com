652e4f698d946df21390fd644976097a588635b3
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-29 03:37:15 +0530

    fixed typo

--- code/deploy.sh
+++ code/deploy.sh
@@ -14,7 +14,7 @@ cd .. && echo "$ignorecontent" > .gitignore
 git add .
 git commit -m "Gatsby build ${date_time}"
 git push origin $temp_branch
-echo "Pushed changes to temp branch_..."
+echo "Pushed changes to temp branch..."
 git checkout gatsby
 # TODO: merge temp branch to main here
 echo "Deleting temp branch..."
