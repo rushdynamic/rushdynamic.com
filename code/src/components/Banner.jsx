b276d80370a247a67ba38f35baf882063acc332d
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-29 21:27:36 +0530

    Gatsby build 20211229-212646

--- code/src/components/Banner.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import "@fontsource/yeseva-one"
-import "../styles/banner-styles.scss"
-
-export default function Banner() {
-  return <div className="main-banner">Hi, I'm Gokul</div>
-}
