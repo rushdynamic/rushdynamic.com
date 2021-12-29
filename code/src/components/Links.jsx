a2f5ccb4338c1fdafabb1e6dcf81728c144e9313
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-30 01:20:12 +0530

    added link icons

---
+++ code/src/components/Links.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "../styles/links-styles.scss"
+
+export default function Links() {
+  return (
+    <div className="links-container">
+      <a href="https://github.com/RushDynamic/" target="_blank">
+        <img src="icons/gh.png" height="24px" />
+      </a>
+      <a href="https://www.linkedin.com/in/gokulsyam/" target="_blank">
+        <img src="icons/li.png" height="24px" />
+      </a>
+    </div>
+  )
+}
--- code/src/pages/index.js
+++ code/src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import "../styles/global-styles.scss"
 import Header from "../components/Header"
 import Banner from "../components/Banner"
+import Links from "../components/Links"
 
 export default function Home() {
   return (
     <div className="global-container">
       <Header />
       <Banner />
+      <Links />
     </div>
   )
 }
