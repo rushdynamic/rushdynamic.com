60536b1be022f1eaca5423186c77888218c329e0
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-31 00:07:16 +0530

    fixed warnings

--- code/src/components/Links.jsx
+++ code/src/components/Links.jsx
@@ -4,11 +4,19 @@ import "../styles/links-styles.scss"
 export default function Links() {
   return (
     <div className="links-container">
-      <a href="https://github.com/RushDynamic/" target="_blank">
-        <img src="icons/gh.png" height="24px" />
+      <a
+        href="https://github.com/RushDynamic/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="icons/gh.png" height="24px" alt="github" />
       </a>
-      <a href="https://www.linkedin.com/in/gokulsyam/" target="_blank">
-        <img src="icons/li.png" height="24px" />
+      <a
+        href="https://www.linkedin.com/in/gokulsyam/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="icons/li.png" height="24px" alt="linkedin" />
       </a>
     </div>
   )
