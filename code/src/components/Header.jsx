adc1a017ca65c815ca7fa4dbc8eb9d8cefbce988
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-31 13:45:37 +0530

    added return to home link to header

--- code/src/components/Header.jsx
+++ code/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import { navigate } from "gatsby"
 import "../styles/header-styles.scss"
 
 export default function Header(props) {
   return (
     <div className="header-container">
-      <h4>Rush Dynamic</h4>
+      <h4 onClick={() => navigate("/")}>Rush Dynamic</h4>
     </div>
   )
 }
