8908d132a4aeb5f9b12c535bbda4b7750c34e15c
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-01-02 21:47:34 +0530

    added missing echo flag

--- code/deploy.sh
+++ code/deploy.sh
@@ -22,51 +22,51 @@ NC="\e[0m"
 
 # perform build
 function build_local() {
-    echo "${BOLD}${CYAN}Starting Gatsby build ${date_time}${NC}"
-    echo "${BOLD}${CYAN}Installing dependencies using npm${NC}"
+    echo -e "${BOLD}${CYAN}Starting Gatsby build ${date_time}${NC}"
+    echo -e "${BOLD}${CYAN}Installing dependencies using npm${NC}"
     npm install 
-    echo "${BOLD}${LIGHTGREEN}Successfully installed dependencies${NC}"
+    echo -e "${BOLD}${LIGHTGREEN}Successfully installed dependencies${NC}"
     gatsby build 
-    echo "${BOLD}${LIGHTGREEN}Finished building${NC}"
+    echo -e "${BOLD}${LIGHTGREEN}Finished building${NC}"
 }
 
 # copy static files
 function copy_static_files() {
-    echo "${BOLD}${CYAN}Copying generated files...${NC}"
+    echo -e "${BOLD}${CYAN}Copying generated files...${NC}"
     cp -af public/. ../.
     cp CNAME ../CNAME
-    echo "${BOLD}${LIGHTGREEN}Finished copying${NC}"
+    echo -e "${BOLD}${LIGHTGREEN}Finished copying${NC}"
 }
 
 # create and push to temp branch
 function push_to_temp() {
-    echo "${BOLD}${CYAN}Creating a new branch...${NC}"
+    echo -e "${BOLD}${CYAN}Creating a new branch...${NC}"
     git checkout -b $temp_branch
     ignorecontent="code/"
-    cd .. && echo "$ignorecontent" > .gitignore
+    cd .. && echo -e "$ignorecontent" > .gitignore
     git rm -r code/ # ignore already committed 'code' dir
     git add .
     git commit -m "Gatsby build ${date_time}"
     git push origin $temp_branch
-    echo "${BOLD}${LIGHTGREEN}Successfully pushed changes to temp branch${NC}"
+    echo -e "${BOLD}${LIGHTGREEN}Successfully pushed changes to temp branch${NC}"
     wait
 }
 
 
 # merge temp branch to main
 function merge_to_master() {
-    echo "${BOLD}${CYAN}Pushing changes to main branch...${NC}"
+    echo -e "${BOLD}${CYAN}Pushing changes to main branch...${NC}"
     git merge -m "Merging main into $temp_branch" -s ours main
     git checkout main
     git merge $temp_branch
     git push origin main
-    echo "${BOLD}${LIGHTGREEN}Successfully pushed changes to main branch${NC}"
+    echo -e "${BOLD}${LIGHTGREEN}Successfully pushed changes to main branch${NC}"
 }
 
 # local cleanup
 function clean_local() {
     git checkout develop
-    echo "${BOLD}${CYAN}Deleted temp branch, deleting all local generated files...${NC}"
+    echo -e "${BOLD}${CYAN}Deleted temp branch, deleting all local generated files...${NC}"
     touch blankfile
     rm -vr !(code|.git)
     wait
@@ -74,10 +74,10 @@ function clean_local() {
 
 # delete temp branch
 function clean_remote() {
-    echo "${BOLD}${CYAN}Deleting temp branch...${NC}"
+    echo -e "${BOLD}${CYAN}Deleting temp branch...${NC}"
     git push origin --delete $temp_branch
-    echo "${BOLD}${LIGHTGREEN}Finished cleanup"
-    echo "Deployment completed successfully${NC}"
+    echo -e "${BOLD}${LIGHTGREEN}Finished cleanup"
+    echo -e "Deployment completed successfully${NC}"
 }
 
 build_local
