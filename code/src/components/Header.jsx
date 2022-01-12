b20e75e9bb6fe878f9b1dac9412e4ee3f8ced87b
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-01-13 00:17:21 +0530

    feat: highlight selected page

--- code/src/components/Header.jsx
+++ code/src/components/Header.jsx
@@ -3,12 +3,29 @@ import { navigate } from "gatsby"
 import "../styles/header-styles.scss"
 
 export default function Header(props) {
+  const curPath = window.location.pathname
   return (
     <div className="header-container">
       <h4 onClick={() => navigate("/")}>Rush Dynamic</h4>
       <div className="pages-container">
-        <h4 onClick={() => navigate("/projects")}>projects</h4>
-        <h4 onClick={() => navigate("/about")}>about</h4>
+        <h4
+          className={
+            curPath.includes("/projects")
+              ? "selected-page"
+              : "non-selected-page"
+          }
+          onClick={() => navigate("/projects")}
+        >
+          projects
+        </h4>
+        <h4
+          className={
+            curPath.includes("/about") ? "selected-page" : "non-selected-page"
+          }
+          onClick={() => navigate("/about")}
+        >
+          about
+        </h4>
       </div>
     </div>
   )
