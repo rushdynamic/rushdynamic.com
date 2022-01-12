ceb0d59b0d8c819b70a291fc99b1e3351293a181
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-01-13 00:23:48 +0530

    fix: window reference

--- code/src/components/Header.jsx
+++ code/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { navigate } from "gatsby"
 import "../styles/header-styles.scss"
 
 export default function Header(props) {
-  const curPath = window.location.pathname
+  const [curPath, setCurPath] = useState("/")
+  useEffect(() => {
+    setCurPath(window?.location.pathname)
+  }, [])
+
   return (
     <div className="header-container">
       <h4 onClick={() => navigate("/")}>Rush Dynamic</h4>
