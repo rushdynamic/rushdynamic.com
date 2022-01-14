c5e2eb7331577e0f0f5546325da99f7a56d00edf
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-01-14 20:57:55 +0530

    feat: added animation to spinner

--- code/src/components/Spinner.jsx
+++ code/src/components/Spinner.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import { motion } from "framer-motion"
 import "../styles/spinner-styles.scss"
 
 export default function Spinner() {
   return (
     <div className="spinner-container">
-      <img src="/spinner.svg" alt="spinner" />
+      <motion.img
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        src="/spinner.svg"
+        alt="spinner"
+      />
     </div>
   )
 }
