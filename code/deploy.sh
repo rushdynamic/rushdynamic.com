0ff18b8f05446ac3362a8ecd69921c5f42a00196
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-01-21 13:42:28 +0530

    fix: implemented shellcheck changes

--- code/deploy.sh
+++ code/deploy.sh
@@ -1,83 +1,77 @@
-#!/bin/sh
+#!/bin/env bash
 
 # TODO: 
-# * add colors to loggers
-# * handle cleanup/local rollback incase of failure
-# * optimize merge to master
+# * accept commandline args
 
 set -e
 shopt -s extglob
 
-date_time=`date "+%Y%m%d-%H%M%S"`
+date_time=$(date "+%Y%m%d-%H%M%S")
 temp_branch="temp_branch_"${date_time}
 
 # colors and text formatting
 BOLD="\e[1m"
-RED="\e[31m"
 LIGHTGREEN="\e[92m"
 CYAN="\e[96m"
-BOLDGREEN="\e[1;${GREEN}m"
-ITALICRED="\e[3;${RED}m"
 NC="\e[0m"
 
 # perform build
-function build_local() {
-    echo -e "${BOLD}${CYAN}Starting Gatsby build ${date_time}"
-    echo -e "Installing dependencies using npm${NC}"
+build_local() {
+    printf "%b%bStarting Gatsby build %s\n" "$BOLD" "$CYAN" "${date_time}" 
+    printf "Installing dependencies using npm%b\n" "$NC"
     npm install 
-    echo -e "${BOLD}${LIGHTGREEN}Successfully installed dependencies${NC}"
+    printf "%b%bSuccessfully installed dependencies%b\n" "$BOLD" "$LIGHTGREEN" "$NC"
     gatsby build 
-    echo -e "${BOLD}${LIGHTGREEN}Finished building${NC}"
+    printf "%b%bFinished building%b\n" "$BOLD" "$LIGHTGREEN" "$NC"
 }
 
 # copy static files
-function copy_static_files() {
-    echo -e "${BOLD}${CYAN}Copying generated files...${NC}"
+copy_static_files() {
+    printf "%b%bCopying generated files...%b\n" "$BOLD" "$CYAN" "$NC"
     cp -af public/. ../.
     cp CNAME ../CNAME
-    echo -e "${BOLD}${LIGHTGREEN}Finished copying${NC}"
+    printf "%b%bFinished copying%b\n" "$BOLD" "$LIGHTGREEN" "$NC"
 }
 
 # create and push to temp branch
-function push_to_temp() {
-    echo -e "${BOLD}${CYAN}Creating a new branch...${NC}"
-    git checkout -b $temp_branch
+push_to_temp() {
+    printf "%b%bCreating a temp branch...%b\n" "$BOLD" "$CYAN" "$NC"
+    git checkout -b "$temp_branch"
     ignorecontent="code/"
-    cd .. && echo -e "$ignorecontent" > .gitignore
+    cd .. && echo "$ignorecontent" > .gitignore
     git rm -r code/ # ignore already committed 'code' dir
     git add .
     git commit -m "Gatsby build ${date_time}"
-    git push origin $temp_branch
-    echo -e "${BOLD}${LIGHTGREEN}Successfully pushed changes to temp branch${NC}"
+    git push origin "$temp_branch"
+    printf "%b%bSuccessfully pushed changes to temp branch%b\n" "$BOLD" "$LIGHTGREEN" "$NC"
     wait
 }
 
-
 # merge temp branch to main
-function merge_to_master() {
-    echo -e "${BOLD}${CYAN}Pushing changes to main branch...${NC}"
+merge_to_master() {
+    printf "%b%bPushing changes to main branch...%b\n" "$BOLD" "$CYAN" "$NC"
     git merge -m "Merging main into $temp_branch" -s ours main
     git checkout main
-    git merge $temp_branch
+    git merge "$temp_branch"
     git push origin main
-    echo -e "${BOLD}${LIGHTGREEN}Successfully pushed changes to main branch${NC}"
+    printf "%b%bSuccessfully merged changes to main branch%b\n" "$BOLD" "$LIGHTGREEN" "$NC"
 }
 
 # local cleanup
-function clean_local() {
+clean_local() {
     git checkout develop
-    echo -e "${BOLD}${CYAN}Deleted temp branch, deleting all local generated files...${NC}"
+    printf "%b%bDeleting all local generated files...%b\n" "$BOLD" "$CYAN" "$NC"
     touch blankfile
     rm -vr !(code|.git)
     wait
 }
 
 # delete temp branch
-function clean_remote() {
-    echo -e "${BOLD}${CYAN}Deleting temp branch...${NC}"
-    git push origin --delete $temp_branch
-    echo -e "${BOLD}${LIGHTGREEN}Finished cleanup"
-    echo -e "Deployment completed successfully${NC}"
+clean_remote() {
+    printf "%b%bDeleting temp branch...%b\n" "$BOLD" "$CYAN" "$NC"
+    git push origin --delete "$temp_branch"
+    printf "%b%bFinished cleanup\n" "$BOLD" "$LIGHTGREEN"
+    printf "Deployment completed successfully%b\n" "$NC"
 }
 
 build_local
