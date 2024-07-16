d78ad2aae38b7d9aa1634265481d17ae0426b7b9
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-16 21:52:06 +0530

    feat: make layout responsive

--- rushdynamic/src/app/NavBar.tsx
+++ rushdynamic/src/app/NavBar.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 interface NavBarItemProps {
 	label: string;
+	title?: boolean;
 }
-const NavBarItem = ({ label }: NavBarItemProps) => {
+const NavBarItem = ({ label, title }: NavBarItemProps) => {
 	return (
 		<div className="before:content-['['] before:invisible after:content-[']'] after:invisible hover:before:visible hover:after:visible hover:cursor-pointer">
 			{label}
@@ -15,7 +16,7 @@ export default function NavBar() {
 	return (
 		<div className="h-12 border-b border-b-dark-grey flex items-center p-2 font-jetbrains-mono">
 			<div className="flex w-full justify-start">
-				<NavBarItem label="gokul.cc" />
+				<NavBarItem label="gokul.cc" title={true} />
 				<div className="flex-grow flex items-center justify-center gap-6">
 					<NavBarItem label="Work" />
 					<NavBarItem label="Contact" />
--- rushdynamic/src/app/page.tsx
+++ rushdynamic/src/app/page.tsx
@@ -4,42 +4,45 @@ import ContactIcons from './ContactIcons';
 
 export default function Home() {
 	return (
-		<main className="h-screen bg-light-grey flex flex-col custom-bg">
+		<main className="min-h-screen bg-light-grey flex flex-col justify-between custom-bg">
 			<NavBar />
 			<div className="h-full flex flex-col items-center justify-center">
-				<div className="text-[12rem] font-neue-metana font-bold select-none text-dark-grey flex flex-col items-center leading-none">
-					<div>Gokul</div>
-					<div>Syam</div>
-					{/* <img src="/images/squiggly.svg" width="100" /> */}
-
-					<div className="flex w-full">
+				<div className="text-7xl md:text-[12vw] font-neue-metana font-bold select-none text-dark-grey flex flex-col items-center leading-none flex-wrap p-2">
+					<div className="grow shrink text-center">Gokul Syam</div>
+					<div className="flex w-full justify-center">
 						<div className="flex-grow"></div>
-						<div className="pr-2">
-							<img src="/images/squiggly.svg" width="100" className="-mr-2" />
+						<div className="p-2">
+							<img src="/images/squiggly.svg" width="100" />
 						</div>
 					</div>
 				</div>
 				{/* <img src="/images/squiggly.svg" /> */}
-				<div className="flex gap-8 items-center p-10 rounded-3xl">
-					<div className="bg-[url('/images/scribble.svg')] h-[350px] w-[350px] bg-no-repeat flex items-center justify-center">
+				<div className="flex flex-col md:flex-row gap-8 items-center p-10 rounded-3xl">
+					<div className="bg-[url('/images/scribble.svg')] bg-center bg-no-repeat flex items-center justify-center p-9">
 						<Image
 							src="/images/headshot.jpg"
 							width="250"
 							height="250"
 							alt="Profile picture"
-							className="border border-light-grey rounded-full select-none "
+							className="border-[3px] border-light-grey rounded-full select-none w-[150px] md:w-[250px]"
 						/>
 					</div>
-					<div className="max-w-[500px] font-montreal text-xl text-dark-grey">
-						<p className="text-2xl">Hi,</p>
+					<div className="md:max-w-[600px] font-montreal text-xl text-dark-grey">
+						<p className="text-2xl pb-3">Hi,</p>
+						<p className="pb-3">
+							I'm a full-stack engineer with over 5 years of professional
+							experience in the industry, currently working out of{' '}
+							<span className="font-fraunces italic">Bangalore, India</span>
+							{'.'}
+						</p>
 						<p>
-							I'm a full-stack engineer working out of{' '}
-							<span className="font-fraunces italic">Bangalore, India</span>{' '}
-							with over 5 years of professional experience in the industry.
+							I'm also an open source enthusiast, hobbyist photographer, and
+							someone who cherishes art in all its forms.
 						</p>
 					</div>
 				</div>
 			</div>
+			{/* <div className="grow" /> */}
 			<ContactIcons />
 		</main>
 	);
