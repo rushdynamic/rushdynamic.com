f179cc827ec034f3939ca542294aa0606ff33a1b
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-19 00:03:05 +0530

    feat: new background noise gradient

--- rushdynamic/src/app/page.tsx
+++ rushdynamic/src/app/page.tsx
@@ -5,26 +5,28 @@ import ContactIcons from './ContactIcons';
 export default function Home() {
 	return (
 		<main className="min-h-screen bg-light-grey flex flex-col justify-between custom-bg">
-			<NavBar />
-			<div className="h-full flex flex-col items-center justify-center">
+			<div className="flex flex-col gap-4">
+				<NavBar />
 				<div className="text-7xl md:text-[12vw] font-neue-metana font-bold select-none text-dark-grey flex flex-col items-center leading-none flex-wrap p-2">
 					<div className="grow shrink text-center">Gokul Syam</div>
-					<div className="flex w-full justify-center">
+					{/* <div className="flex w-fit justify-center">
 						<div className="flex-grow"></div>
 						<div className="p-2">
 							<img src="/images/squiggly.svg" width="100" />
 						</div>
-					</div>
+					</div> */}
 				</div>
-				{/* <img src="/images/squiggly.svg" /> */}
-				<div className="flex flex-col md:flex-row gap-8 items-center p-10 rounded-3xl">
+				<div className="flex-grow"></div>
+			</div>
+			<div className="h-full flex flex-col items-center justify-center">
+				<div className="flex flex-col md:flex-row gap-8 items-center px-10 pb-10 md:pt-10 rounded-3xl">
 					<div className="bg-[url('/images/scribble.svg')] bg-center bg-no-repeat flex items-center justify-center p-9">
 						<Image
 							src="/images/headshot.jpg"
 							width="250"
 							height="250"
 							alt="Profile picture"
-							className="border-[3px] border-light-grey rounded-full select-none w-[150px] md:w-[250px]"
+							className="border-[3px] border-white rounded-full select-none w-[150px] md:w-[250px]"
 						/>
 					</div>
 					<div className="md:max-w-[600px] font-montreal text-xl text-dark-grey">
@@ -36,8 +38,8 @@ export default function Home() {
 							{'.'}
 						</p>
 						<p>
-							I'm also an open source enthusiast, hobbyist photographer, and
-							someone who cherishes art in all its forms.
+							I'm also an open source enthusiast, a hobbyist photographer, and
+							someone who relishes art in all its forms.
 						</p>
 					</div>
 				</div>
