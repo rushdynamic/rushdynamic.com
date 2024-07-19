4ba8f737ea83012435372997675164dcb02898c7
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-20 01:43:52 +0530

    fix: make all project components server components

--- rushdynamic/src/app/projects/ProjectItems.tsx
+++ rushdynamic/src/app/projects/ProjectItems.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
+import Link from 'next/link';
 
 interface ProjectItemTagProps {
 	tagName: string;
@@ -19,9 +19,6 @@ interface ProjectItemProps {
 	tags: string[];
 	git: string;
 }
-interface ProjectItemWithOnClickProps extends ProjectItemProps {
-	onClick: Dispatch<SetStateAction<string>>;
-}
 const ProjectItem = ({
 	title,
 	year,
@@ -29,37 +26,36 @@ const ProjectItem = ({
 	description,
 	tags,
 	git,
-	onClick,
-}: ProjectItemWithOnClickProps) => {
+}: ProjectItemProps) => {
 	return (
-		<div
-			className="group h-fit w-80 min-w-80 p-6 bg-gradient-to-t from-white to-transparent flex flex-col gap-2 justify-between md:border-t border-solid border-dark-grey hover:cursor-pointer hover:w-[400px] transition-all"
-			onClick={() => onClick(git)}
-		>
-			<div className="flex items-center justify-center">
-				<img src={image} className="rounded-sm" />
-			</div>
-			<div className="flex items-center justify-between gap-1">
-				<div className="text-4xl">{title}</div>
-				<div className="text-sm px-2 border border-solid border-dark-grey group-hover:bg-dark-grey group-hover:text-light-grey">
-					{year}
+		<Link href={git} passHref legacyBehavior>
+			<a target="_blank" rel="noopener noreferrer">
+				<div className="group h-fit w-80 min-w-80 p-6 bg-gradient-to-t from-white to-transparent flex flex-col gap-2 justify-between md:border-t border-solid border-dark-grey hover:cursor-pointer hover:w-[400px] transition-all">
+					<div className="flex items-center justify-center">
+						<img src={image} className="rounded-sm" />
+					</div>
+					<div className="flex items-center justify-between gap-1">
+						<div className="text-4xl">{title}</div>
+						<div className="text-sm px-2 border border-solid border-dark-grey group-hover:bg-dark-grey group-hover:text-light-grey">
+							{year}
+						</div>
+					</div>
+					<div className="text-lg">{description}</div>
+					<div className="flex flex-wrap gap-1">
+						{tags.map((tag) => (
+							<ProjectItemTag key={tag} tagName={tag} />
+						))}
+					</div>
 				</div>
-			</div>
-			<div className="text-lg">{description}</div>
-			<div className="flex flex-wrap gap-1">
-				{tags.map((tag) => (
-					<ProjectItemTag key={tag} tagName={tag} />
-				))}
-			</div>
-		</div>
+			</a>
+		</Link>
 	);
 };
 
 interface ProjectItemsProps {
 	projects: ProjectItemProps[];
-	onClick: Dispatch<SetStateAction<string>>;
 }
-const ProjectItems = ({ projects, onClick }: ProjectItemsProps) => {
+const ProjectItems = ({ projects }: ProjectItemsProps) => {
 	return (
 		<div className="w-full flex items-start justify-center gap-2 flex-wrap">
 			{projects.map((project) => (
@@ -71,7 +67,6 @@ const ProjectItems = ({ projects, onClick }: ProjectItemsProps) => {
 					description={project.description}
 					tags={project.tags}
 					git={project.git}
-					onClick={onClick}
 				/>
 			))}
 		</div>
--- rushdynamic/src/app/projects/page.tsx
+++ rushdynamic/src/app/projects/page.tsx
@@ -1,23 +1,17 @@
-'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import projects from '../data/projects.json';
 import ProjectItems from './ProjectItems';
-import ExpandedProjectItem from './ExpandedProjectItem';
 
 export default function Projects() {
-	const [activeProject, setActiveProject] = useState<string>('');
-	console.log('activeProject:', activeProject);
-	if (activeProject == '')
-		return (
-			<div className="p-8 font-montreal text-dark-grey flex flex-col gap-4">
-				<div className="text-7xl font-semibold">Projects</div>
-				<div className="text-xl md:text-2xl lg:text-3xl">
-					Independent projects I've worked on for both learning and amusement.
-					<br />
-					Primarily for amusement.
-				</div>
-				<ProjectItems projects={projects} onClick={setActiveProject} />
+	return (
+		<div className="p-8 font-montreal text-dark-grey flex flex-col gap-4">
+			<div className="text-7xl font-semibold">Projects</div>
+			<div className="text-xl md:text-2xl lg:text-3xl">
+				Independent projects I've worked on for both learning and amusement.
+				<br />
+				Primarily for amusement.
 			</div>
-		);
-	return <ExpandedProjectItem onClear={() => setActiveProject('')} />;
+			<ProjectItems projects={projects} />
+		</div>
+	);
 }
