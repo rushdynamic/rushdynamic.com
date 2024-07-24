306fcfad051dedd468f03fc243ed3e6f5b64f5cd
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-25 00:45:53 +0530

    chore: updated personal description

--- rushdynamic/src/app/page.tsx
+++ rushdynamic/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 						initial={{ y: 20, opacity: 0 }}
 						animate={{ y: 0, opacity: 1 }}
 						transition={{ ease: 'easeInOut', duration: 0.5 }}
-						className="group border-t border-solid border-dark-grey bg-gradient-to-t from-[transparent] to-[transparent] flex items-center justify-center p-8 md:p-12"
+						className="group border-t border-solid border-dark-grey bg-gradient-to-t from-[transparent] to-[transparent] flex items-center justify-center py-8 md:py-12 cursor-help"
 					>
 						<div className="flex relative w-[250px] h-[250px] md:w-[500px] md:h-[500px] xl:w-[700px] xl:h-[700px]">
 							<Image
@@ -30,8 +30,11 @@ export default function Home() {
 								layout="fill"
 								objectFit="cover"
 								alt="Profile picture"
-								className="select-none w-[250px] md:w-[700px] transition-opacity duration-1000 ease-out opacity-100 hover:opacity-0"
+								className="select-none w-[250px] md:w-[700px] transition-opacity duration-[2000ms] ease-out opacity-100 group-hover:opacity-0"
 							/>
+							<div className="absolute bottom-2 left-2 opacity-0 group-active:opacity-100 text-white text-4xl md:text-6xl lg:text-9xl font-montreal select-none transition-opacity duration-200">
+								New Delhi / 2019
+							</div>
 						</div>
 					</motion.div>
 					<motion.div
@@ -45,11 +48,11 @@ export default function Home() {
 							{
 								"I'm a full-stack engineer with over 5 years of professional experience in the industry, currently working out of "
 							}
-							<span className="font-fraunces italic">Bangalore, India</span>
+							<span className="font-fraunces italic">Bangalore, India</span>.
 						</p>
 						<p>
 							{
-								"I'm also an open source enthusiast, a hobbyist photographer, and someone who relishes art in all its forms."
+								"I'm passionate about what I do, and I'm driven by the challenge of solving difficult, real-world problems."
 							}
 						</p>
 					</motion.div>
