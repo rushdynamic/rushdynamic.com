712fbff348baf393887e03eba0fb65fabdc3d2b7
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-25 14:55:55 +0530

    fix: footer icon links

--- rushdynamic/src/app/Footer.tsx
+++ rushdynamic/src/app/Footer.tsx
@@ -1,27 +1,41 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 interface ContactIconProps {
 	img: string;
 	alt: string;
+	url: string;
 }
-const ContactIcon = ({ img, alt }: ContactIconProps) => {
+const ContactIcon = ({ img, alt, url }: ContactIconProps) => {
 	return (
-		<Image
-			className="opacity-30 hover:opacity-100 transition-opacity cursor-pointer"
-			src={img}
-			alt={alt}
-			height={20}
-			width={20}
-		/>
+		<Link href={url} passHref legacyBehavior>
+			<a target="_blank" rel="noopener noreferrer">
+				<Image
+					className="opacity-30 hover:opacity-100 transition-opacity cursor-pointer"
+					src={img}
+					alt={alt}
+					height={20}
+					width={20}
+				/>
+			</a>
+		</Link>
 	);
 };
 
 export default function Footer() {
 	return (
 		<div className="flex w-full justify-center gap-8 pb-4">
-			<ContactIcon img="/images/icons/github.svg" alt="GitHub" />
-			<ContactIcon img="/images/icons/linkedin.svg" alt="LinkedIn" />
+			<ContactIcon
+				img="/images/icons/github.svg"
+				alt="GitHub"
+				url="https://github.com/rushdynamic"
+			/>
+			<ContactIcon
+				img="/images/icons/linkedin.svg"
+				alt="LinkedIn"
+				url="https://www.linkedin.com/in/gokulsyam/"
+			/>
 		</div>
 	);
 }
